f7750c74fefe12853fd9c9d5c56f1ee72735e8ce
saipriya23 <saipriyareddy2307@gmail.com>
2018-12-12 21:50:05 +0530

    emp

---
+++ EmployeeList/Controllers/EmployeeDetailsController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using EmployeeList.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeList.Controllers
+{
+    public class EmployeeDetailsController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            var Employee = new List<EmployeeForm>();
+            using (
+                var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44380/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("GetEmployeeDetails");
+
+
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    var readTask = result.Content.ReadAsAsync<List<EmployeeForm>>();
+
+                    Employee = readTask.Result;
+                }
+                return View(Employee);
+
+
+            }
+
+        }
+        
+        public IActionResult ChangeEmpType()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ChangeEmpType(EmployeeForm emp)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:64189/api/EmployeeList");
+
+                //HTTP POST
+                var postTask = client.PostAsJsonAsync<EmployeeForm>("", emp);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+
+            return View(emp);
+        }
+            
+        }
+    }
---
+++ EmployeeList/Models/EmployeeForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeList.Models
+{
+    public class EmployeeForm
+    {
+        public string EmployeeName { get; set; }
+        public string EmployeeType { get; set; }
+    }
+}
